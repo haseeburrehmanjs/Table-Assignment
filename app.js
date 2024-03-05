16e7629610e9f2076e031a09649fbf9b9eb76b22
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-03-05 12:42:08 +0500

    modifed work

---
+++ app.js
@@ -0,0 +1,26 @@
+// function sumNum() {
+//     let totalMarks = `Totla Marks ==> ${400}`
+//     console.log(totalMarks);
+//     var obtainNumber = parseInt(input1.value) + parseInt(input2.value) + parseInt(input3.value) + parseInt(input4.value);
+//     console.log(`obtain Marks ==> ${obtainNumber}`);
+//     console.log(obtainNumber/totalMarks*100);
+//     input1.value = ''
+//     input2.value = ''
+//     input3.value = ''
+//     input4.value = ''
+// }
+
+function sumNum() {
+    let totalMarks = 400; // Assuming total marks is 400
+    console.log(`Total Marks ==> ${totalMarks}`);
+    var obtainNumber = Number(input1.value) + Number(input2.value) + Number(input3.value) + Number(input4.value) ;
+    console.log(`Obtain Marks ==> ${obtainNumber}`);
+    let percentage = (obtainNumber / totalMarks) * 100;
+    console.log(`Percentage: ${percentage}%`);
+    input1.value = '';
+    input2.value = '';
+    input3.value = '';
+    input4.value = '';
+}
+
+
