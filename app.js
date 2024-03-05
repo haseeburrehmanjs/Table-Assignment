debb088995217eaf80eed065469e413c622d0e19
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-03-05 19:39:46 +0500

    modied work

--- app.js
+++ app.js
@@ -13,7 +13,7 @@
 function sumNum() {
     let totalMarks = 400; // Assuming total marks is 400
     console.log(`Total Marks ==> ${totalMarks}`);
-    var obtainNumber = Number(input1.value) + Number(input2.value) + Number(input3.value) + Number(input4.value) ;
+    var obtainNumber = Number(input1.value) + Number(input2.value) + Number(input3.value) + Number(input4.value);
     console.log(`Obtain Marks ==> ${obtainNumber}`);
     let percentage = (obtainNumber / totalMarks) * 100;
     console.log(`Percentage: ${percentage}%`);
@@ -21,6 +21,15 @@ function sumNum() {
     input2.value = '';
     input3.value = '';
     input4.value = '';
+    if (percentage >= 1 && 30 >= percentage){
+        console.log(`Bad News You Are Fail!`);
+    }else if (percentage >= 31 && 50 >= percentage){
+        console.log(`Need Work Hard "C" Grade`);
+    }else if (percentage >= 51 && 70 >= percentage){
+        console.log(`Congratulation "B" Grade`);
+    }else {
+        console.log(`Congratulation "A" grade"`);
+    }
 }
 
 
