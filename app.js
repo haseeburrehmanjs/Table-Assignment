b9addddc9839393640e46d8ca16c28739046f2dc
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-03-06 22:11:48 +0500

    Modified work

--- app.js
+++ app.js
@@ -10,25 +10,32 @@
 //     input4.value = ''
 // }
 
+
+var studentTotal = document.querySelector('#total-marks')
+var percentage = document.querySelector('#percentage')
+var grade = document.querySelector('#grade')
+
 function sumNum() {
     let totalMarks = 400; // Assuming total marks is 400
     console.log(`Total Marks ==> ${totalMarks}`);
-    var obtainNumber = Number(input1.value) + Number(input2.value) + Number(input3.value) + Number(input4.value);
+    let obtainNumber = Number(input1.value) + Number(input2.value) + Number(input3.value) + Number(input4.value);
     console.log(`Obtain Marks ==> ${obtainNumber}`);
-    let percentage = (obtainNumber / totalMarks) * 100;
+    let studentPercentage = (obtainNumber / totalMarks) * 100;
     console.log(`Percentage: ${percentage}%`);
+    studentTotal.innerHTML = obtainNumber;
+    percentage.innerHTML = studentPercentage
     input1.value = '';
     input2.value = '';
     input3.value = '';
     input4.value = '';
-    if (percentage >= 1 && 30 >= percentage){
-        console.log(`Bad News You Are Fail!`);
-    }else if (percentage >= 31 && 50 >= percentage){
-        console.log(`Need Work Hard "C" Grade`);
-    }else if (percentage >= 51 && 70 >= percentage){
-        console.log(`Congratulation "B" Grade`);
+    if (studentPercentage > 80){
+        grade.innerHTML = `A+`
+    }else if (studentPercentage > 60){
+        grade.innerHTML = `B`
+    }else if (studentPercentage > 30){
+        grade.innerHTML = `C`
     }else {
-        console.log(`Congratulation "A" grade"`);
+        grade.innerHTML = `FAIL`
     }
 }
 
